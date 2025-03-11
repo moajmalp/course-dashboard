db7024117e455bef78eab64575b293914166a441
MUHAMMED AJMAL P <165270501+moajmalp@users.noreply.github.com>
2025-03-12 03:39:00 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "codo-course-dashboard-v1";
+const STATIC_ASSETS = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/script.js",
+    "/favicon.ico",
+];
+
+// Install Service Worker & Cache Files
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(STATIC_ASSETS);
+        })
+    );
+});
+
+// Activate Service Worker & Remove Old Cache
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Data from Cache when Offline
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
